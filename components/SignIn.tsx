fdc636cae6a25a4c986770aa495e33d422b1559c
Harshalvk <harshalvkhobragade@gmail.com>
2025-02-03 22:22:30 +0530

    feat: add & setup prisma orm with edge compatibility

--- components/SignIn.tsx
+++ components/SignIn.tsx
@@ -5,7 +5,7 @@ export default function SignIn() {
     <form
       action={async () => {
         "use server";
-        await signIn("google");
+        await signIn("google", { redirectTo: "/" });
       }}
     >
       <button type="submit">Signin with Google</button>
