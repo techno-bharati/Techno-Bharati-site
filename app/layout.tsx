89733425dc46a7f4e0dedcbd3f16c07af8528cb8
Harshalvk <harshalvkhobragade@gmail.com>
2025-02-03 20:30:57 +0530

    add: reconfig next-theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Navigation />
-        {children}
+        <AppProvider>{children}</AppProvider>
       </body>
     </html>
   );
