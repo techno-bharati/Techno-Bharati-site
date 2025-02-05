cd8c1334521e04dc475d3f39b659c854f5c576b4
Harshalvk <harshalvkhobragade@gmail.com>
2025-02-05 22:51:31 +0530

    add: admin prisma migrations

---
+++ prisma/migrations/20250205160918_admin/migration.sql
@@ -0,0 +1,19 @@
+-- CreateEnum
+CREATE TYPE "AdminRole" AS ENUM ('SUPER_ADMIN', 'EVENT_ADMIN');
+
+-- CreateTable
+CREATE TABLE "Admin" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "role" "AdminRole" NOT NULL DEFAULT 'EVENT_ADMIN',
+    "eventType" "EventType",
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Admin_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Admin_email_key" ON "Admin"("email");
