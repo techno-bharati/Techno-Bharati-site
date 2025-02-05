9c2dcff23b009df7c0c0743a22a6846d3c0446e6
Harshalvk <harshalvkhobragade@gmail.com>
2025-02-05 17:09:27 +0530

    add: config react-query & sonner

--- components/providers/AppProvider.tsx
+++ components/providers/AppProvider.tsx
@@ -1,11 +1,26 @@
 "use client";
 
 import { ThemeProvider } from "next-themes";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { useState } from "react";
+import { Toaster } from "../ui/sonner";
 
 export function AppProvider({ children }: { children: React.ReactNode }) {
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
-    <ThemeProvider attribute={"class"} defaultTheme="dark" enableSystem>
-      {children}
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        {children}
+        <Toaster />
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
