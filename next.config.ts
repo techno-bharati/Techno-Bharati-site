6eeec33c84b0a3f42e45aa3dec84a8741eec1ed6
Harshalvk <harshalvkhobragade@gmail.com>
2025-02-05 21:04:48 +0530

    fix: next config file for cloudinary image

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "hebbkx1anhila5yf.public.blob.vercel-storage.com",
       },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
     ],
   },
 };
