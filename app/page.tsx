5ff31d9fe8c3e6086d2015f0a5fee6c2011b5d93
Harshalvk <harshalvkhobragade@gmail.com>
2025-02-22 18:24:09 +0530

    feat: Integrate ImageCarousel into landing page

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,13 @@ import { Features } from "@/components/landing/Features";
 import { Footer } from "@/components/landing/Footer";
 import { Hero2 } from "@/components/landing/Hero2";
 import { Sponsors } from "@/components/landing/Sponsors";
+import ImageCarousel from "@/components/landing/ImageCarousel";
 
 export default function Home() {
   return (
     <>
       <Hero2 />
+      <ImageCarousel />
       <EventCards />
       <Sponsors />
       <Features />
