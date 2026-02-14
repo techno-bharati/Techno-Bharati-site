2c1c5996b111dbc808449e1aaf82443e6be7285f
Harshalvk <harshalvkhobragade@gmail.com>
2026-02-14 19:35:36 +0530

    feat: smooth scroll provider

--- components/providers/AppProvider.tsx
+++ components/providers/AppProvider.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import { ThemeProvider } from "next-themes";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { useState } from "react";
 import { Toaster } from "../ui/sonner";
+import { ThemeProvider } from "./ThemeProvider";
 
 export function AppProvider({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(() => new QueryClient());
---
+++ components/providers/SmoothScrollProvider.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import Lenis from "lenis";
+import { useEffect } from "react";
+
+export function SmoothScroll({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      smoothWheel: true,
+    });
+
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+
+  return <>{children}</>;
+}
