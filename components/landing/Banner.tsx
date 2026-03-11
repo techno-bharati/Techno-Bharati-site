a764273e1610188f552c643269f461309db56630
Harshalvk <harshalvkhobragade@gmail.com>
2026-03-11 14:07:35 +0530

    chore: update banner image

--- components/landing/Banner.tsx
+++ components/landing/Banner.tsx
@@ -36,7 +36,13 @@ const Banner = () => {
       className="max-w-5xl mx-auto my-5 px-4"
     >
       <img
-        src={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/f_auto,q_auto,w_auto,dpr_auto/v1772962566/pl9hadguvdqgorhtbczr.jpg`}
+        src={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/f_auto,q_auto,w_1920/v1772962566/diy0ing3xujkmjfkeap0.jpg`}
+        srcSet={`
+    https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/f_auto,q_auto,w_640/v1772962566/diy0ing3xujkmjfkeap0.jpg 640w,
+    https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/f_auto,q_auto,w_1024/v1772962566/diy0ing3xujkmjfkeap0.jpg 1024w,
+    https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/f_auto,q_auto,w_1920/v1772962566/diy0ing3xujkmjfkeap0.jpg 1920w
+  `}
+        sizes="(max-width: 640px) 640px, (max-width: 1024px) 1024px, 1920px"
         alt="banner"
         className="h-full w-full shadow-md rounded-2xl"
         loading="eager"
